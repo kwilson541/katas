e77a7239dcc4e39873cc8e005ab7e1b34daee2d9
Kim Wilson <k.wilson541@gmail.com>
2016-11-10 09:48:14 +0000

    Added Fizzbuzz Round 9

---
+++ fizzbuzz/lib/fizzbuzz.rb
@@ -0,0 +1,19 @@
+def divisible_by_3?(number)
+	number % 3 == 0
+end
+
+def divisible_by_5?(number)
+	number % 5 == 0
+end
+
+def divisible_by_3_and_5?(number)
+	divisible_by_3?(number) == true && divisible_by_5?(number) == true
+end
+
+def fizzbuzz(number)
+	return 0 if number == 0
+	return "Fizzbuzz" if divisible_by_3_and_5?(number)
+	return "Fizz" if divisible_by_3?(number)
+	return "Buzz" if divisible_by_5?(number)
+	return number
+end
